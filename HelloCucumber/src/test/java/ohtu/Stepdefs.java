7ade334b5b577d7d546ccc7389f901910e0e5dcb
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2017-04-02 23:04:30 +0300

    tests

--- HelloCucumber/src/test/java/ohtu/Stepdefs.java
+++ HelloCucumber/src/test/java/ohtu/Stepdefs.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,6 +23,12 @@ public class Stepdefs {
     public void it_is_incremented_by(int val) throws Throwable {
         counter.increment(val);
     }     
+    
+    @When("^it is reseted$")
+    public void it_is_reseted() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        counter.reset();
+    }
 
     @Then("^the value should be (\\d+)$")
     public void the_value_should_be(int val) throws Throwable {
--- LoginCucumber/src/test/java/ohtu/Stepdefs.java
+++ LoginCucumber/src/test/java/ohtu/Stepdefs.java
@@ -17,6 +17,21 @@ public class Stepdefs {
     AuthenticationService auth = new AuthenticationService(userDao);
     List<String> inputLines = new ArrayList<>();
     
+    @Given("^user \"([^\"]*)\" with password \"([^\"]*)\" is created$")
+    public void user_with_password_is_created(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        inputLines.add("new");
+        inputLines.add(arg1);
+        inputLines.add(arg2);
+    }
+
+    
+    @Given("^command new user is selected$")
+    public void command_new_user_is_selected() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        inputLines.add("new");
+    }
+    
     @Given("^command login is selected$")
     public void command_login_selected() throws Throwable {
         inputLines.add("login");
@@ -36,4 +51,5 @@ public class Stepdefs {
     public void system_will_respond_with(String expectedOutput) throws Throwable {
         assertTrue(io.getPrints().contains(expectedOutput));
     }
+    
 }
